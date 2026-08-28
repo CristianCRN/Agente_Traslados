c6dff4d650626cfeef9fe5e78f081f473b5c3da4
Cristian Lopez <cristian241965@live.com>
2026-08-28 16:47:50 -0500

    Migrate from SQLite to PostgreSQL Supabase

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from bot.Telegram_bot import main
 from database.modelos import inicializar
-import asyncio
 import threading
+import requests
+import time
 from flask import Flask
 
 app_flask = Flask(__name__)
@@ -13,9 +14,17 @@ def home():
 def correr_flask():
     app_flask.run(host='0.0.0.0', port=8080)
 
+def mantener_activo():
+    time.sleep(30)
+    while True:
+        try:
+            requests.get("https://agente-traslados.onrender.com")
+        except:
+            pass
+        time.sleep(600)
+
 if __name__ == "__main__":
     inicializar()
-    # Flask corre en hilo separado
     threading.Thread(target=correr_flask, daemon=True).start()
-    # Bot corre en el hilo principal
+    threading.Thread(target=mantener_activo, daemon=True).start()
     main()
